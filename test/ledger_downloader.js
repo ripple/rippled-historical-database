f715a03f3a2aee6bfe13314e6ed72b7199677d15
Kevin Johnson <kevinejohn@gmail.com>
2014-07-25 15:47:54 -0700

    [FEATURE] Added ledger downloader. Added unique indexes

---
+++ test/ledger_downloader.js
@@ -0,0 +1,26 @@
+var assert = require('assert');
+var LedgerDownloader = require('../lib/ledger_downloader');
+
+describe('Ledger Downloader', function() {
+
+  var ledger_downloader = new LedgerDownloader();
+
+  it('should get latest ledger index', function(done) {
+    ledger_downloader.latestIndex(function(err, index) {
+      if (err) return err;
+      assert(typeof index === 'number');
+      assert(index > 0);
+      done();
+    });
+  });
+
+  it('should get a particular ledger index', function(done) {
+    ledger_downloader.getIndex(7000001, function(err, ledger) {
+      if (err) return err;
+      assert(typeof ledger === 'object');
+      done();
+    });
+  });
+
+});
+
