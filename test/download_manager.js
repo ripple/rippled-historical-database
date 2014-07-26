687be7c970505702e718e4727994eb325abe5248
Kevin Johnson <kevinejohn@gmail.com>
2014-07-25 17:26:13 -0700

    [FEATURE] Retrieves ledgers oldest first

---
+++ test/download_manager.js
@@ -0,0 +1,12 @@
+var assert = require('assert');
+var DownloadManager = require('../lib/download_manager');
+
+describe('Download Manager', function() {
+
+  var dm = new DownloadManager();
+
+  it('should start downloading ledgers', function() {
+
+  });
+
+});
