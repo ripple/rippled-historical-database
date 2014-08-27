30a890fc435a5427c7bc95ed22283c8db78ff4c5
Mark Travis <mtravis@ripple.com>
2014-08-27 16:08:10 -0700

    read file input, fix exception handling

--- import/import2.py
+++ import/import2.py
@@ -14,8 +14,9 @@ import sys
 argparser = argparse.ArgumentParser()
 argparser.add_argument("-c", "--connection", type=str, required=True)
 argparser.add_argument("-p" "--pgconnection", type=str, required=True)
-argparser.add_argument("-s", "--start", type=int, required=True)
 argparser.add_argument("-l", "--logfile", type=str, required=True)
+argparser.add_argument("-i", "--inputfile", type=str)
+argparser.add_argument("-s", "--start", type=int)
 argparser.add_argument("-e", "--end", type=int)
 argparser.add_argument("-t", "--timeout", type=int, default=60)
 args = argparser.parse_args()
@@ -28,6 +29,53 @@ pgconn = psycopg2.connect(args.p__pgconnection)
 pgconn.set_session(autocommit=True)
 
 logfile = open(args.logfile, "a", 1)
+
+if hasattr(args, "inputfile"):
+    with open(args.inputfile, "r") as infile:
+        for i in infile:
+            i = i.rstrip()
+
+            breakout = False
+            while True:
+                if breakout:
+                    break;
+
+                logfile.write(str(datetime.datetime.now()) + " fetching ledger " + i\
+                    + "\n")
+                ledger = None
+                try:
+                    ledger = ripd.cmd_ledger(i, transactions=True, expand=True)
+                    logfile.write(str(datetime.datetime.now()) +
+                              " fetched ledger " + str(i) + "\n")
+                    break;
+                except pyripple.RippleError as err:
+                    logfile.write(str(err) + "\n")
+                    logfile.write(str(sys.exc_info()) + "\n")
+                    if err.error == "lgrNotFound":
+                        breakout = True
+                        continue # log this, go to next
+                    time.sleep(2)
+                except Exception as err:
+                    if hasattr(err, "remote_ip"):
+                        logfile.write("remote_ip: " + err.remote_ip)
+                    logfile.write(str(err) + "\n")
+                    logfile.write(str(sys.exc_info()) + "\n")
+                    time.sleep(20) # log & stuff & try again, must be socket-related
+
+            if breakout == True:
+                continue
+
+            logfile.write(str(datetime.datetime.now()) + " storing ledger " + i\
+                + "\n")
+            try:
+                sl = storeledger.StoreLedger(pgconn, ledger["result"]["ledger"])
+                logfile.write(str(datetime.datetime.now()) + " stored ledger "
+                              + i + "\n")
+            except:
+                logfile.write(str(sys.exc_info()) + "\n")
+                time.sleep(2)
+
+    sys.exit(0)
 
 for i in range(args.start, args.end+1):
     logfile.write(str(datetime.datetime.now()) + " fetching ledger " + str(i)\
