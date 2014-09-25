617abe0777ffcd90e381ac933edfae45e257c3e7
Vahakn Papazian <vahakn@Vahakns-MacBook-Pro.local>
2014-09-25 10:08:09 -0700

    added accounts table

--- src/db.js
+++ src/db.js
@@ -8,6 +8,10 @@ var bookshelf = require('bookshelf')(knex);
 var Promise = require('bluebird');
 var db = { };
 
+var SerializedObject = require('ripple-lib').SerializedObject;
+//test = new SerializedObject.from_json(i).to_hex();
+
+
 var Ledger = bookshelf.Model.extend({
 	tableName: 'ledgers',
 	idAttribute: null
@@ -25,20 +29,24 @@ var Account = bookshelf.Model.extend({
 
 //Main
 db.saveLedger = function (ledger, callback) {
-	 
 	//Print
 	//console.log(ledger);
-	//console.log(ledger.transactions);
+	//console.log(ledger.transactions[0]);
 
 	//Add all transactions to an array
 	toAdd = parse_transactions(ledger);
 
-	//Add ledger info to database
+	//Add ledger info to array
 	ledger_info = parse_ledger(ledger);
 	toAdd.push(ledger_info);
 
-	console.log(toAdd.length - 1);
+	//Testing atomic queries with corrupt data
+	//var monkey_wrench = Transaction.forge({id:5});
+	//toAdd.push(monkey_wrench);
+
+	console.log(toAdd.length - 1 + " transactions.");
 	insert_all(toAdd);
+
 };
 
 //Given an array of queries, add to database atomically
@@ -59,15 +67,24 @@ function parse_transactions(ledger){
 	var transaction_list = [];
 	for (var i=0; i<ledger.transactions.length; i++){
 		var entry = ledger.transactions[i];
+		var meta = entry.metaData;
+		var tx = entry;
+
+		delete tx.metaData;
+		var hex_tx = to_hex(tx);
+		var hex_meta = to_hex(meta);
+
+		console.log(hex_tx, hex_meta);
+
 		var tranaction_info = Transaction.forge({
 			hash: entry.hash,
 			type: entry.TransactionType,
 			account: entry.Account,
 			sequence: entry.Sequence,
 			ledger_index: ledger.seqNum,
-			result: entry.metaData.TransactionResult,
-			//raw:,
-			meta: entry.metaData,
+			result: meta.TransactionResult,
+			//raw: hex_tx,
+			//meta: hex_meta
 		});
 		transaction_list.push(tranaction_info);
 	}
@@ -90,5 +107,9 @@ function parse_ledger(ledger){
 	return ledger_info;
 }
 
+function to_hex(input){
+	hex = new SerializedObject.from_json(input).to_hex();
+	return hex;
+}
 
 module.exports = db;
