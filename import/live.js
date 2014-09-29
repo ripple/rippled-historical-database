931fefe655899c55ef668427e40d5e866a44a9d0
Vahakn Papazian <vahakn@Vahakns-MacBook-Pro.local>
2014-09-29 14:25:07 -0700

    fixed missing meta problem

--- import/live.js
+++ import/live.js
@@ -3,11 +3,13 @@ var Importer = require('./importer');
 var live     = new Importer();
 var indexer  = require('./couchdb/indexer');
 var couchdb  = require('./couchdb/client');
+var config  = require('../config/import.config');
+var postgres = new require('../lib/db.js')(config.get('sql'));
 
 live.liveStream();
 live.on('ledger', function(ledger) {
-  
-  couchdb.saveLedger(ledger, function(err, resp){
+	postgres.saveLedger(ledger);
+/*  couchdb.saveLedger(ledger, function(err, resp){
     if (resp) indexer.pingCouchDB();
-  });
+  });*/
 });
