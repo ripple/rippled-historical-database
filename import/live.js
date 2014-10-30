417a37b8ac7ca5434affa05d56cce6a8c711a052
Matthew Fettig <matthew@ripple.com>
2014-10-30 15:45:50 -0700

    config arg for importer destination

--- import/live.js
+++ import/live.js
@@ -1,3 +1,4 @@
+var config   = require('../config/import.config');
 var log      = require('../lib/log')('ledgerstream');
 var Importer = require('./importer');
 var live     = new Importer();
@@ -6,28 +7,46 @@ var couchdb  = require('./couchdb/client');
 var postgres = new require('./postgres/client');
 //var hbase    = require('./hbase/client');
 
+var typeList = config.get('type') || 'postgres';
+var types    = { };
+typeList = typeList.split(',');
+typeList.forEach(function(type) {
+  types[type] = true;
+});
 
 //start import stream
 live.liveStream();
 
-/*
+
 //hbase importer
-live.on('ledger', function(ledger) {
-  hbase.saveLedger(ledger);
-});
-*/
+if (types.hbase) {
+  log.info('Saving Ledgers to HBase');
+  live.on('ledger', function(ledger) {
+    hbase.saveLedger(ledger);
+  });
+}
+
 
 //postgres importer
-live.on('ledger', function(ledger) {
-  postgres.saveLedger(ledger);
-});
-
-
-/*
-//couchdb importer
-live.on('ledger', function(ledger) {
-  couchdb.saveLedger(ledger, function(err, resp){
-    if (resp) indexer.pingCouchDB();
+if (types.postgres) {
+  log.info('Saving Ledgers to Postgres');
+  live.on('ledger', function(ledger) {
+    postgres.saveLedger(ledger, function(err, resp){
+      if (err) {
+        log.error('error saving ledger:', err);
+      } else {
+        log.info('Ledger Saved:', resp.ledger_index);
+      }
+    });
   });
-});
-*/
+}
+
+//couchdb importer
+if (types.couchdb) {
+  log.info('Saving Ledgers to CouchDB');
+  live.on('ledger', function(ledger) {
+    couchdb.saveLedger(ledger, function(err, resp){
+      if (resp) indexer.pingCouchDB();
+    });
+  });
+}
