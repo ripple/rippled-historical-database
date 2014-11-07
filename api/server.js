e236e10fe09dbe26e144a65596b1d4a24ba5fcff
Matthew Fettig <matthew@ripple.com>
2014-11-06 17:08:51 -0800

    allow cors

--- api/server.js
+++ api/server.js
@@ -4,9 +4,11 @@ var express    = require('express');
 var bodyParser = require('body-parser');
 var routes     = require('./routes');
 var db         = new require('./lib/db.js')(config.get('sql'));
+var cors       = require('cors');
 
 var app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 //define routes
 app.get('/v1/accounts/:address/transactions', routes.accountTx);
