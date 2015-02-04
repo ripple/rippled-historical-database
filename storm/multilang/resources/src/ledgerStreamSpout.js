ebcd4e8810b34207016f5d1a98bef95196dce42f
Matthew Fettig <matthew@ripple.com>
2015-02-04 13:41:57 -0800

    implement retry for failed transactions up to 10 times

--- storm/multilang/resources/src/ledgerStreamSpout.js
+++ storm/multilang/resources/src/ledgerStreamSpout.js
@@ -12,6 +12,7 @@ var ledgerSpout;
 
 function LedgerStreamSpout() {
   Spout.call(this); 
+  this.pending = { };
 };
 
 LedgerStreamSpout.prototype = Object.create(Spout.prototype);
@@ -44,7 +45,14 @@ LedgerStreamSpout.prototype.nextTuple = function(done) {
     //call the function immedately
     //if there are more transactions to process
     if (stream.transactions.length) timeout = 0;
-
+    
+    //keep the transaction around
+    //until its acked
+    self.pending[id] = {
+      attempts : 1,
+      tx       : tx
+    };
+    
     //emit transaction
     self.emit({tuple:[tx], id:id}, function(taskIds){
       self.log('tx: ' + id + ' sent to - ' + taskIds);
@@ -54,18 +62,38 @@ LedgerStreamSpout.prototype.nextTuple = function(done) {
   setTimeout(done, timeout);
 };
 
+/**
+ * ack
+ * report a transaction as success
+ * and remove it from the pending list
+ */
+
 LedgerStreamSpout.prototype.ack = function(id, done) {
   this.log('Received ack for - ' + id);
-  //delete this.pending[id];
+  delete this.pending[id];
   done();
 };
 
+/**
+ * fail
+ * report a transaction as failed
+ * retry it up to 10 times
+ */
+
 LedgerStreamSpout.prototype.fail = function(id, done) {
   var self = this;
-  this.log('Received FAIL for - ' + id);
-  //this.emit({tuple: this.pending[id], id:id}, function(taskIds) {
-  //    self.log(self.pending[id] + ' sent to task ids - ' + taskIds);
-  //});
+
+  if (++self.pending[id].attempts <= 10) {
+    self.log('Received FAIL for - ' + id + ' Retrying, attempt #' + self.pending[id].attempts);
+    self.emit({tuple: self.pending[id].tx, id:id}, function(taskIds) {
+        self.log('tx: ' + id + ' resent to - ' + taskIds);
+    });
+    
+  } else {
+    self.log('Received FAIL for - ' + id + ' - Stopping after 10 attempts');
+    delete self.pending[id];
+  }
+  
   done();
 };
 
