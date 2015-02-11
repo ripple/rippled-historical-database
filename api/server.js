3875748d3a8db1ccbe5236198242d076748fdacf
vahaknp <vahakn@papazian.info>
2015-02-10 17:36:15 -0800

    removed slash

--- api/server.js
+++ api/server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 //define routes
-app.get('/v1/accounts/:address/transactions/', routes.accountTx);
+app.get('/v1/accounts/:address/transactions', routes.accountTx);
 app.get('/v1/accounts/:address/transactions/:sequence?', routes.accountTxSeq);
 app.get('/v1/ledgers/:ledger_param?', routes.getLedger);
 app.get('/v1/accounts/:address/balances', routes.accountBalances);
