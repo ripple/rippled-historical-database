86d6d55e9afa9ca06bf930605d7ec632f9f8ff7d
Matthew Fettig <matthew@ripple.com>
2015-02-13 13:22:03 -0800

    prefer expand over binary for ledgers endpoint

--- api/routes/getLedger.js
+++ api/routes/getLedger.js
@@ -48,8 +48,8 @@ var getLedger = function (req, res, next) {
       else options.err = {error:"invalid ledger identifier", code:400};
     }
     
-    if (options.binary) options.tx_return = 'binary';
-    else if (options.expand) options.tx_return = 'json';
+    if (options.expand) options.tx_return = 'json';
+    else if (options.binary) options.tx_return = 'binary';
     else if (options.transactions) options.tx_return = 'hex';
     else options.tx_return = 'none';
 
@@ -62,8 +62,8 @@ var getLedger = function (req, res, next) {
   * @param {Object} err
   */
   function errorResponse (err) {
+    log.error(err.error || err);
     if (err.code.toString()[0] === '4') {
-      log.error(err.error || err);
       response.json({result:'error', message:err.error}).status(err.code).pipe(res);
     } else {
       response.json({result:'error', message:'unable to retrieve ledger'}).status(500).pipe(res);  
