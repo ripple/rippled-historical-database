0c114b4d3f65956db9e7ee3a13fb7e05d1e83ec7
Matthew Fettig <matthew@ripple.com>
2015-02-18 12:26:46 -0800

    user ripple-lib-transactionparser for payment balance changes

--- storm/multilang/resources/src/lib/modules/ledgerParser/payment.js
+++ storm/multilang/resources/src/lib/modules/ledgerParser/payment.js
@@ -1,8 +1,14 @@
+var Amount     = require('ripple-lib').Amount;
+var BigNumber  = require('bignumber.js');
 var XRP_ADJUST = 1000000.0;
+var parseBalanceChanges   = require('ripple-lib-transactionparser').parseBalanceChanges;
+var parseOrderBookChanges = require('ripple-lib-transactionparser').parseOrderBookChanges;
 
 var Payments = function (tx) {
   var payment = { };
   var amount;
+  var balanceChanges;
+  var sourceBalanceChanges;
   
   if (tx.metaData.TransactionResult !== 'tesSUCCESS') {
     return null;
@@ -17,10 +23,24 @@ var Payments = function (tx) {
     return null;
   }
   
+  //parse balance changes
+  balanceChanges = parseBalanceChanges(tx.metaData);
+  
   payment.source      = tx.Account;
   payment.destination = tx.Destination;
-  payment.source_balance_changes = [];
-  payment.destination_balance_changes = [];
+  payment.source_balance_changes      = [];
+  payment.destination_balance_changes = balanceChanges[tx.Destination];
+
+  balanceChanges[tx.Account].forEach(function(change) {
+    if (change.currency === 'XRP') {
+      var fee      = new BigNumber (tx.Fee).dividedBy(XRP_ADJUST).negated();
+      change.value = new BigNumber(change.value).minus(fee).toString();
+    }
+    
+    if (change.value !== '0') {
+      payment.source_balance_changes.push(change);
+    }
+  });
   
   if (tx.DestionationTag) {
     payment.destination_tag = tx.DestionationTag;
@@ -37,16 +57,16 @@ var Payments = function (tx) {
     
   } else {
     payment.currency = 'XRP';
-    payment.amount   = tx.Amount / XRP_ADJUST;
+    payment.amount   = new BigNumber(tx.Amount).dividedBy(XRP_ADJUST).toString();
   }
   
   //delivered amount fields
   amount = tx.metaData.DeliveredAmount || tx.Amount;
   if (typeof amount === 'object') {
-    payment.delivered_amount   = amount.value;
-    
+    payment.delivered_amount = amount.value;
+
   } else {
-    payment.delivered_amount   = amount / XRP_ADJUST;
+    payment.delivered_amount = new BigNumber(amount).dividedBy(XRP_ADJUST).toString();
   }  
   
   //max amount
@@ -55,103 +75,15 @@ var Payments = function (tx) {
     payment.source_currency = tx.SendMax.currency;
     
   } else if (tx.SendMax) {
-    payment.max_amount = tx.SendMax / XRP_ADJUST;
+    payment.max_amount      = new BigNumber(tx.SendMax).dividedBy(XRP_ADJUST).toString();
     payment.source_currency = 'XRP';
   }
-  
-  
-  //get balance changes for the sender and receiver
-  tx.metaData.AffectedNodes.forEach(function(affNode, i) {
-    var node = affNode.ModifiedNode || affNode.CreatedNode || affNode.DeletedNode;
-    var fields;
-    var final;
-    var previous;
-    var amount;
-    var highParty;
-    var lowParty;
-    var sendAmount;
-    
-    if (!node) {
-      return;
-    }
-    
-    fields = node.FinalFields || node.NewFields;
-    
-    if (!fields) {
-      return;
-    }
-    
-    if (node.LedgerEntryType === "AccountRoot") {
-    
-      final    = fields.Balance;
-      previous = node.PreviousFields ? node.PreviousFields.Balance : 0;
-      change   = final - previous;
-      
-      if (fields.Account === tx.Destination) {
-        payment.destination_balance_changes.push({
-          currency : 'XRP',
-          amount   : change / XRP_ADJUST
-        });
-        
-        
-      } else if (fields.Account === tx.Account) {
-        var fee = parseInt(tx.Fee, 10);
-        change += fee;
-        if (change) {
-          payment.source_balance_changes.push({
-            currency : 'XRP',
-            amount   : change / XRP_ADJUST
-          });
-        }
-
-      } else {
-        return;
-      }
-      
-    } else if (node.LedgerEntryType === 'RippleState') {
-
-      highParty = fields.HighLimit.issuer;
-      lowParty  = fields.LowLimit.issuer;
-      previous  = node.PreviousFields ? parseFloat(node.PreviousFields.Balance.value) : 0;
-      final     = parseFloat(fields.Balance.value);
-      change    = final - previous;
-      
-      if (final < 0 || previous < 0) {
-        issuer  = lowParty;
-        account = highParty;
-        final   = 0 - final;
-        change  = 0 - change;
-        
-      } else {
-        issuer  = highParty;
-        account = lowParty;
-      }
-      
-      if (tx.Account === account ||
-          tx.Account === issuer) {
-        payment.source_balance_changes.push({
-          currency : fields.Balance.currency,
-          issuer   : issuer,
-          amount   : change
-        });  
-      }
-
-      if (tx.Destination === account ||
-          tx.Destination === issuer) {
-        payment.destination_balance_changes.push({
-          currency : fields.Balance.currency,
-          issuer   : issuer,
-          amount   : change
-        });  
-      }
-    }   
-  });
   
   if (!payment.source_currency && payment.source_balance_changes.length) {
     payment.source_currency = payment.source_balance_changes[0].currency;
   }
   
-  payment.fee          = parseInt(tx.Fee, 10) / XRP_ADJUST;
+  payment.fee          = new BigNumber (tx.Fee).dividedBy(XRP_ADJUST).toString();
   payment.ledger_index = tx.ledger_index;
   payment.tx_index     = tx.tx_index;
   payment.time         = tx.executed_time;
