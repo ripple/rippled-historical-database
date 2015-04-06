802e728a3d11e0aa09686eb7aeeaf307ee9a3c65
Gilles Pirio <gilles@ripple.com>
2015-04-06 16:38:00 -0700

    Tests for payments, exchanges, including pagination

--- test/online.test.js
+++ test/online.test.js
@@ -3,8 +3,11 @@ var Parser  = require('../lib/ledgerParser');
 var Rest    = require('../lib/hbase/hbase-rest');
 var HBase   = require('../lib/hbase/hbase-client');
 var Promise = require('bluebird');
+var request  = require('request');
 var fs      = require('fs');
+var Server   = require('../api/server');
 var PREFIX  = 'TEST_' + Math.random().toString(36).substr(2, 5) + '_';
+var port    = 7111;
 
 var rest = new Rest({
   prefix : PREFIX,
@@ -22,6 +25,13 @@ var options = {
 var hbase = new HBase(options);
 var path  = __dirname + '/ledgers/';
 var files = fs.readdirSync(path);
+
+server = new Server({
+  postgres : undefined,
+  hbase    : options,
+  port     : port,
+});
+
 
 describe('HBASE client and API endpoints', function () {
   before(function(done){
@@ -57,6 +67,157 @@ describe('HBASE client and API endpoints', function () {
       console.log(resp.length, 'ledgers saved');
       done();
     });
+  });
+
+  it('should make sure /v1/accounts/:account is not a valid endpoint', function(done) {
+    var url = 'http://localhost:' + port + '/v1/accounts/rfU3YWd1TnYryvryQTQ9xwyCSqzMTbnyW6';
+    request({
+      url: url,
+      json: true,
+    },
+    function (err, res, body) {
+      assert.ifError(err);
+      assert.strictEqual(res.statusCode, 404);
+      done();
+    });
+  });
+
+  // This function helps build tests that iterate trough a list of results
+  //
+  function checkPagination(baseURL, initalMarker, testFunction, done) {
+    var url= baseURL + (initalMarker ? '&marker='+initalMarker : '');
+    request({
+        url: url,
+        json: true,
+      },
+        function (err, res, body) {
+          assert.ifError(err);
+          assert.strictEqual(res.statusCode, 200);
+          assert.strictEqual(typeof body, 'object');
+          assert.strictEqual(body.result, 'success');
+          return checkIter(body, 0, body.count, initalMarker);
+    });
+    
+    function checkIter(ref, i, imax, marker) {
+      if(i < imax) {
+        var url= baseURL + (marker ? '&limit=1&marker='+marker : '&limit=1');
+        request({
+            url: url,
+            json: true,
+          },
+            function (err, res, body) {
+              assert.ifError(err);
+              assert.strictEqual(res.statusCode, 200);
+              assert.strictEqual(typeof body, 'object');
+              assert.strictEqual(body.result, 'success');
+              testFunction(ref, i, body);
+              checkIter(ref, i+1, imax, body.marker);
+        });
+      } else {
+        done();
+      }
+    }
+  }
+
+  it('should make sure /v1/accounts/:account/payments handles limit correctly', function(done) {
+    var url = 'http://localhost:' + port + '/v1/accounts/rpjZUBy92h6worVCYERZcVCzgzgmHb17Dx/payments?limit=2';
+    request({
+      url: url,
+      json: true,
+    },
+    function (err, res, body) {
+      assert.ifError(err);
+      assert.strictEqual(res.statusCode, 200);
+      assert.strictEqual(body.count, 2);
+      assert.strictEqual(body.payments.length, 2);      
+      done();
+    });    
+  });
+
+  it('should make sure /v1/accounts/:account/payments handles pagination correctly', function(done) {
+    this.timeout(5000);
+    var url = 'http://localhost:' + port + '/v1/accounts/rpjZUBy92h6worVCYERZcVCzgzgmHb17Dx/payments?';
+    checkPagination(url, undefined, function(ref, i, body) {
+      assert.strictEqual(body.payments.length, 1);
+      assert.equal(body.payments[0].amount, ref.payments[i].amount);
+      assert.equal(body.payments[0].tx_hash, ref.payments[i].tx_hash);
+    }, done);
+  });
+
+  it('should make sure /v1/accounts/:account/exhanges handles limit correctly', function(done) {
+    var url = 'http://localhost:' + port + '/v1/accounts/rHsZHqa5oMQNL5hFm4kfLd47aEMYjPstpg/exchanges?limit=5';
+    request({
+      url: url,
+      json: true,
+    },
+    function (err, res, body) {
+      assert.ifError(err);
+      assert.strictEqual(res.statusCode, 200);
+      assert.strictEqual(body.count, 5);
+      assert.strictEqual(body.exchanges.length, 5);      
+      done();
+    });    
+  });
+
+  it('should make sure /v1/accounts/:account/exhanges/:curr handles currency correctly', function(done) {
+    var url = 'http://localhost:' + port + '/v1/accounts/rHsZHqa5oMQNL5hFm4kfLd47aEMYjPstpg/exchanges/jpy';
+    request({
+      url: url,
+      json: true,
+    },
+    function (err, res, body) {
+      assert.ifError(err);
+      assert.strictEqual(res.statusCode, 200);
+      body.exchanges.forEach( function(exch) {
+        assert.strictEqual(exch.base_currency, 'JPY');
+      });
+      done();
+    });    
+  });
+
+  it('should make sure /v1/accounts/:account/exhanges/:curr handles currency correctly', function(done) {
+    var url = 'http://localhost:' + port + '/v1/accounts/rHsZHqa5oMQNL5hFm4kfLd47aEMYjPstpg/exchanges/BTC';
+    request({
+      url: url,
+      json: true,
+    },
+    function (err, res, body) {
+      assert.ifError(err);
+      assert.strictEqual(res.statusCode, 200);
+      body.exchanges.forEach( function(exch) {
+        assert.strictEqual(exch.base_currency, 'BTC');
+      });
+      done();
+    });    
+  });
+
+  it('should make sure /v1/accounts/:account/exhanges/:curr-iss/:counter handles parameters correctly', function(done) {
+    var url = 'http://localhost:' + port + '/v1/accounts/rHsZHqa5oMQNL5hFm4kfLd47aEMYjPstpg/exchanges/USD+rMwjYedjc7qqtKYVLiAccJSmCwih4LnE2q/xrp';
+    request({
+      url: url,
+      json: true,
+    },
+    function (err, res, body) {
+      assert.ifError(err);
+      assert.strictEqual(res.statusCode, 200);
+      body.exchanges.forEach( function(exch) {
+        assert.strictEqual(exch.base_currency, 'USD');
+        assert.strictEqual(exch.base_issuer, 'rMwjYedjc7qqtKYVLiAccJSmCwih4LnE2q');
+        assert.strictEqual(exch.counter_currency, 'XRP');        
+      });
+      done();
+    });    
+  });
+
+  it('should make sure /v1/accounts/:account/exhanges handles pagination correctly', function(done) {
+    this.timeout(5000);
+    var url = 'http://localhost:' + port + '/v1/accounts/rHsZHqa5oMQNL5hFm4kfLd47aEMYjPstpg/exchanges?';
+    checkPagination(url, undefined, function(ref, i, body) {
+      assert.strictEqual(body.exchanges.length, 1);
+      assert.equal(body.exchanges[0].base_amount, ref.exchanges[i].base_amount);
+      assert.equal(body.exchanges[0].base_currency, ref.exchanges[i].base_currency);      
+      assert.equal(body.exchanges[0].tx_hash, ref.exchanges[i].tx_hash);
+    }, done);
   });
 
   after(function(done) {
