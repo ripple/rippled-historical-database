8c305fab4ff9542f1265829849b7c73da9cdae7d
Matthew Fettig <matthew@ripple.com>
2015-06-11 15:46:27 -0700

    handle invalid time formats

--- api/routesV2/stats.js
+++ api/routesV2/stats.js
@@ -43,6 +43,12 @@ var Stats = function(req, res) {
       }
     }
 
+    if (!options.start) {
+      return {error: 'invalid start time, must be ISO_8601', code: 400};
+    } else if (!options.end) {
+      return {error: 'invalid end time, must be ISO_8601', code: 400};
+    }
+
     return options;
   }
 
