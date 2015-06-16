b04f1d1f4c0fe41fc2185c06cefdf33231f901b7
Matthew Fettig <matthew@ripple.com>
2015-06-16 13:26:00 -0700

    add stats aggregation to tests

--- test/hbaseTests/importLedgers.js
+++ test/hbaseTests/importLedgers.js
@@ -6,20 +6,29 @@ var HBase = require('../../lib/hbase/hbase-client');
 var Promise = require('bluebird');
 var moment = require('moment');
 var exAggregation = require('../../lib/aggregation/exchanges');
+var statsAggregation = require('../../lib/aggregation/stats');
+
 var fs = require('fs');
 var path = __dirname + '/../ledgers/';
 var files = fs.readdirSync(path);
 var hbaseConfig = config.get('hbase');
+var statsConfig;
+var updates = [];
+var exchanges = [];
+var pairs = { };
 var hbase;
+var stats;
+
 
 hbaseConfig.prefix = config.get('prefix') || 'TEST_';
 hbaseConfig.logLevel = 2;
 hbaseConfig.max_sockets = 200;
 hbaseConfig.timeout = 30000;
 
+statsConfig = JSON.parse(JSON.stringify(hbaseConfig));
+statsConfig.logLevel = 4;
+stats = new statsAggregation(statsConfig);
 hbase = new HBase(hbaseConfig);
-var exchanges = [];
-var pairs = { };
 
 describe('import ledgers', function(done) {
   it('should save ledgers into hbase', function(done) {
@@ -29,9 +38,24 @@ describe('import ledgers', function(done) {
         var ledger = JSON.parse(fs.readFileSync(path + filename, "utf8"));
         var parsed = Parser.parseLedger(ledger);
 
+        //save exchanges
+        exchanges.push.apply(exchanges, parsed.exchanges);
+
+        //save stats
+        addStats(parsed);
+        updates.push({
+          label : 'ledger_count',
+          data  : {
+            time         : ledger.close_time,
+            ledger_index : ledger.ledger_index,
+            tx_count     : ledger.transactions.length
+          }
+        });
+        resolve();
+        return;
+
         hbase.saveParsedData({data:parsed}, function(err, resp) {
           assert.ifError(err);
-          exchanges.push.apply(exchanges, parsed.exchanges);
           hbase.saveTransactions(parsed.transactions, function(err, resp) {
             assert.ifError(err);
             hbase.saveLedger(parsed.ledger, function(err, resp) {
@@ -76,4 +100,79 @@ describe('import ledgers', function(done) {
       });
     });
   });
+
+  it('should save stats into hbase', function(done) {
+    this.timeout(6000);
+    updates.forEach(function(u) {
+      stats.update(u);
+    });
+    setTimeout(done, 5000);
+  });
 });
+
+function addStats (parsed) {
+
+  //save transaction stats
+  parsed.transactions.forEach(function(tx) {
+      //increment transactions
+    updates.push({
+      label : 'transaction_count',
+      data  : {
+        count  : 1,
+        time   : tx.executed_time
+      }
+    });
+
+    //aggregate by transaction type
+    updates.push({
+      label : 'transaction_type',
+      data  : {
+        type   : tx.TransactionType,
+        time   : tx.executed_time
+      }
+    });
+
+    //aggregate by transaction result
+    updates.push({
+      label : 'transaction_result',
+      data  : {
+        result : tx.tx_result,
+        time   : tx.executed_time
+      }
+    });
+  });
+
+  //new account created
+  parsed.accountsCreated.forEach(function(a) {
+    updates.push({
+      label : 'accounts_created',
+      data  : {
+        count  : 1,
+        time   : a.time
+      }
+    });
+  });
+
+  //aggregate payments count
+  parsed.payments.forEach(function(p) {
+    updates.push({
+      label : 'payments_count',
+      data  : {
+        count  : 1,
+        time   : p.time
+      }
+    });
+  });
+
+
+  //aggregate exchanges count
+  parsed.exchanges.forEach(function(ex) {
+    updates.push({
+      label : 'exchanges_count',
+      data  : {
+        count  : 1,
+        time   : ex.time
+      }
+    });
+  });
+}
