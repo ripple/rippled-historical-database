2d363001e82ffc836cc4b6d453541436c530fcc6
Matthew Fettig <matthew@ripple.com>
2015-06-23 13:57:57 -0700

    script for adding genesis accounts

---
+++ import/hbase/genesisAccounts.js
@@ -0,0 +1,35 @@
+'use strict';
+var config = require('../../config/import.config');
+var Hbase = require('../../lib/hbase/hbase-client');
+var utils = require('../../lib/utils');
+var BigNumber = require('bignumber.js');
+var moment = require('moment');
+var ledger = require('../../lib/32570.json');
+var hbase = new Hbase(config.get('hbase'));
+var count = 0;
+var time = utils.formatTime(ledger.close_time_human);
+var unix = moment.utc(ledger.close_time_human).unix();
+
+ledger.accountState.forEach(function(state, i) {
+  var row;
+  var rowkey;
+
+  if (state.LedgerEntryType === 'AccountRoot') {
+    count++;
+    row = {
+      'f:account' : state.Account,
+      'f:genesis_balance' : new BigNumber(state.Balance).dividedBy(1000000).toString(),
+      'f:executed_time' : unix,
+      'f:ledger_index' : ledger.ledger_index,
+      'd:genesis_index' : count
+    };
+
+    rowkey = time + '|' + utils.padNumber(ledger.ledger_index, 12) + '|genesis|' + utils.padNumber(count, 3);
+    hbase.putRow('accounts_created', rowkey, row)
+    .nodeify(function(err, resp) {
+      console.log(rowkey, err, resp);
+    });
+  }
+});
+
+console.log(count + ' Accounts');
