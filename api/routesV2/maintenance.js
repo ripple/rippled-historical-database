2f343faf00e677045a0348df6cef1dee1a143a42
Matthew Fettig <matthew@ripple.com>
2015-11-18 11:29:07 -0800

    add maintenance/banner endpoint for ripplecharts

---
+++ api/routesV2/maintenance.js
@@ -0,0 +1,81 @@
+'use strict';
+
+var Logger = require('../../lib/logger');
+var log = new Logger({scope: 'maintenance'});
+var domains = ['ripplecharts'];
+var hbase;
+
+/**
+ * Stats
+ */
+
+var getMaintenance = function(req, res) {
+  var table = 'control';
+  var rowkey = 'maintenance_' + req.params.domain;
+
+  if (domains.indexOf(req.params.domain) === -1) {
+    errorResponse({
+      error: 'Invalid domain',
+      code: 400
+    });
+    return;
+  }
+
+  hbase.getRow({
+    table: table,
+    rowkey: rowkey
+  }, function(err, resp) {
+    if (err) {
+      errorResponse(err);
+    } else {
+      successResponse(resp);
+    }
+  });
+
+  /**
+  * errorResponse
+  * return an error response
+  * @param {Object} err
+  */
+
+  function errorResponse(err) {
+    log.error(err.error || err);
+    if (err.code && err.code.toString()[0] === '4') {
+      res.status(err.code).json({
+        result: 'error',
+        message: err.error
+      });
+    } else {
+      res.status(500).json({
+        result: 'error',
+        message: 'unable to get data'
+      });
+    }
+  }
+
+  /**
+  * successResponse
+  * return a successful response
+  * @param {Object} payments
+  */
+
+  function successResponse(resp) {
+    var result = {
+      result: 'success',
+      mode: resp && resp.mode ? resp.mode : 'normal'
+    };
+
+    if (result.mode === 'maintenance' ||
+        result.mode === 'banner') {
+      result.html = resp.html;
+      result.style = resp.style ? JSON.parse(resp.style) : undefined;
+    }
+
+    res.json(result);
+  }
+};
+
+module.exports = function(db) {
+  hbase = db;
+  return getMaintenance;
+};
