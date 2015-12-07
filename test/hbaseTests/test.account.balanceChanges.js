e05a9c3567da714d70983a2a14061c48ff7f5bf5
Matthew Fettig <matthew@ripple.com>
2015-12-07 13:12:04 -0800

    update account balance changes API and tests

--- test/hbaseTests/test.account.balanceChanges.js
+++ test/hbaseTests/test.account.balanceChanges.js
@@ -7,8 +7,10 @@ var port = config.get('port') || 7111;
 
 describe('account balance changes API endpoint', function() {
 
-  it('should make sure /accounts/:account/balance_changes handles limit correctly', function(done) {
-    var url = 'http://localhost:' + port + '/v2/accounts/rpjZUBy92h6worVCYERZcVCzgzgmHb17Dx/balance_changes?limit=2';
+  it('should handle limit correctly', function(done) {
+    var url = 'http://localhost:' + port +
+      '/v2/accounts/rpjZUBy92h6worVCYERZcVCzgzgmHb17Dx/balance_changes?limit=2';
+
     request({
       url: url,
       json: true,
@@ -22,8 +24,10 @@ describe('account balance changes API endpoint', function() {
     });
   });
 
-  it('should make sure /accounts/:account/balance_changes handles currency correctly', function(done) {
-    var url = 'http://localhost:' + port + '/v2/accounts/rpjZUBy92h6worVCYERZcVCzgzgmHb17Dx/balance_changes?currency=xrp';
+  it('should handle currency correctly', function(done) {
+    var url = 'http://localhost:' + port +
+      '/v2/accounts/rpjZUBy92h6worVCYERZcVCzgzgmHb17Dx/balance_changes?currency=xrp';
+
     request({
       url: url,
       json: true,
@@ -39,8 +43,12 @@ describe('account balance changes API endpoint', function() {
     });
   });
 
-  it('should make sure /accounts/:account/balance_changes handles currency correctly', function(done) {
-    var url = 'http://localhost:' + port + '/v2/accounts/rHsZHqa5oMQNL5hFm4kfLd47aEMYjPstpg/balance_changes?currency=btc';
+  it('should handle counterparty correctly', function(done) {
+    var counterparty = 'rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B';
+    var url = 'http://localhost:' + port +
+      '/v2/accounts/rHsZHqa5oMQNL5hFm4kfLd47aEMYjPstpg/balance_changes?' +
+      'currency=btc&counterparty=' + counterparty;
+
     request({
       url: url,
       json: true,
@@ -51,33 +59,17 @@ describe('account balance changes API endpoint', function() {
       assert.notStrictEqual(body.balance_changes.length, 0);  // Make sure we test something
       body.balance_changes.forEach( function(bch) {
         assert.strictEqual(bch.currency, 'BTC');
+        assert.strictEqual(bch.counterparty, counterparty);
       });
       done();
     });
   });
 
-  it('should make sure /accounts/:account/balance_changes handles currency correctly', function(done) {
-    var issuer= 'rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B';
-    var url = 'http://localhost:' + port + '/v2/accounts/rHsZHqa5oMQNL5hFm4kfLd47aEMYjPstpg/balance_changes?'
-                                         + 'currency=btc&issuer='+issuer;
-    request({
-      url: url,
-      json: true,
-    },
-    function (err, res, body) {
-      assert.ifError(err);
-      assert.strictEqual(res.statusCode, 200);
-      assert.notStrictEqual(body.balance_changes.length, 0);  // Make sure we test something
-      body.balance_changes.forEach( function(bch) {
-        assert.strictEqual(bch.currency, 'BTC');
-        assert.strictEqual(bch.issuer, issuer);
-      });
-      done();
-    });
-  });
+  it('should limit results by change type', function(done) {
+    var url = 'http://localhost:' + port +
+      '/v2/accounts/rHsZHqa5oMQNL5hFm4kfLd47aEMYjPstpg/balance_changes?' +
+      'change_type=transaction_cost';
 
-  it('should make sure /accounts/:account/balance_changes handles currency correctly', function(done) {
-    var url = 'http://localhost:' + port + '/v2/accounts/rHsZHqa5oMQNL5hFm4kfLd47aEMYjPstpg/balance_changes?currency=XRP';
     request({
       url: url,
       json: true,
@@ -88,14 +80,17 @@ describe('account balance changes API endpoint', function() {
       assert.notStrictEqual(body.balance_changes.length, 0);  // Make sure we test something
       body.balance_changes.forEach( function(bch) {
         assert.strictEqual(bch.currency, 'XRP');
+        assert.strictEqual(bch.change_type, 'transaction_cost');
       });
       done();
     });
   });
 
-  it('should make sure /accounts/:account/balance_changes handles pagination correctly', function(done) {
+  it('should handle pagination correctly', function(done) {
     this.timeout(5000);
-    var url = 'http://localhost:' + port + '/v2/accounts/rpjZUBy92h6worVCYERZcVCzgzgmHb17Dx/balance_changes?';
+    var url = 'http://localhost:' + port +
+      '/v2/accounts/rpjZUBy92h6worVCYERZcVCzgzgmHb17Dx/balance_changes?';
+
     utils.checkPagination(url, undefined, function(ref, i, body) {
       assert.strictEqual(body.balance_changes.length, 1);
       assert.equal(body.balance_changes[0].change, ref.balance_changes[i].change);
@@ -104,9 +99,12 @@ describe('account balance changes API endpoint', function() {
     }, done);
   });
 
-  it('should make sure /accounts/:account/balance_changes handles pagination correctly (descending)', function(done) {
+  it('should handle pagination correctly (descending)', function(done) {
     this.timeout(5000);
-    var url = 'http://localhost:' + port + '/v2/accounts/rpjZUBy92h6worVCYERZcVCzgzgmHb17Dx/balance_changes?descending=true';
+    var url = 'http://localhost:' + port +
+      '/v2/accounts/rpjZUBy92h6worVCYERZcVCzgzgmHb17Dx/balance_changes' +
+      '?descending=true';
+
     utils.checkPagination(url, undefined, function(ref, i, body) {
       assert.strictEqual(body.balance_changes.length, 1);
       assert.equal(body.balance_changes[0].change, ref.balance_changes[i].change);
@@ -115,9 +113,9 @@ describe('account balance changes API endpoint', function() {
     }, done);
   });
 
-  it('should make sure /accounts/:account/balance_changes handles dates correctly', function(done) {
-    var start= '2015-01-14T18:00:00';
-    var end= '2015-01-14T18:30:00';
+  it('should handle dates correctly', function(done) {
+    var start = '2015-01-14T18:00:00';
+    var end = '2015-01-14T18:30:00';
     var url = 'http://localhost:' + port + '/v2/accounts/rpjZUBy92h6worVCYERZcVCzgzgmHb17Dx/balance_changes?'
                                          + 'start=' + start + '&end='+ end;
     request({
@@ -136,10 +134,10 @@ describe('account balance changes API endpoint', function() {
     });
   });
 
-  it('should make sure /accounts/:account/balance_changes handles descending correctly', function(done) {
+  it('should handle descending correctly', function(done) {
     var url = 'http://localhost:' + port +
-        '/v2/accounts/rMwjYedjc7qqtKYVLiAccJSmCwih4LnE2q/balance_changes?' +
-        'descending=true';
+      '/v2/accounts/rMwjYedjc7qqtKYVLiAccJSmCwih4LnE2q/balance_changes?' +
+      'descending=true';
 
     request({
       url: url,
@@ -161,9 +159,9 @@ describe('account balance changes API endpoint', function() {
     });
   });
 
-  it('should make sure /accounts/:account/balance_changes handles empty response correctly', function(done) {
-    var start= '1015-01-14T18:00:00';
-    var end= '1970-01-14T18:30:00';
+  it('should handle empty response correctly', function(done) {
+    var start = '1015-01-14T18:00:00';
+    var end = '1970-01-14T18:30:00';
     var url = 'http://localhost:' + port + '/v2/accounts/rpjZUBy92h6worVCYERZcVCzgzgmHb17Dx/balance_changes?'
                                          + 'start=' + start + '&end='+ end;
     request({
@@ -179,24 +177,27 @@ describe('account balance changes API endpoint', function() {
     });
   });
 
-  it('should make sure /accounts/:account/balance_changes handles empty response correctly', function(done) {
-    var url = 'http://localhost:' + port + '/v2/accounts/rrrrUBy92h6worVCYERZcVCzgzgmHb17Dx/balance_changes';
+  it('should handle invalid params correctly', function(done) {
+    var url = 'http://localhost:' + port +
+      '/v2/accounts/rrrrUBy92h6worVCYERZcVCzgzgmHb17Dx/balance_changes' +
+      '?counterparty=rpjZUBy92h6worVCYERZcVCzgzgmHb17Dx&currency=Xrp';
+
     request({
       url: url,
       json: true,
     },
     function (err, res, body) {
       assert.ifError(err);
-      assert.strictEqual(res.statusCode, 200);
-      assert.strictEqual(body.balance_changes.length, 0);
-      assert.strictEqual(body.count, 0);
+      assert.strictEqual(res.statusCode, 400);
       done();
     });
   });
 
-  it('should make sure /accounts/:account/balance_changes handles empty invalid params correctly', function(done) {
-    var url = 'http://localhost:' + port + '/v2/accounts/rrrrUBy92h6worVCYERZcVCzgzgmHb17Dx/balance_changes?'
-                                         + 'issuer=rpjZUBy92h6worVCYERZcVCzgzgmHb17Dx&currency=Xrp' ;
+  it('should handle invalid change_type', function(done) {
+    var url = 'http://localhost:' + port +
+      '/v2/accounts/rHsZHqa5oMQNL5hFm4kfLd47aEMYjPstpg/balance_changes?' +
+      'change_type=zzz';
+
     request({
       url: url,
       json: true,
