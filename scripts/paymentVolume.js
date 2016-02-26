2a7ecb23fa2822e0ced162f9c4976cf503653e40
Matthew Fettig <matthew@ripple.com>
2016-02-26 15:48:52 -0800

    filter components with no value

--- scripts/paymentVolume.js
+++ scripts/paymentVolume.js
@@ -259,8 +259,12 @@ function handleAggregation(params, done) {
       }, function (err, resp) {
         if (err) {
           reject(err);
+
+        // filter rows with no payments
         } else {
-          resolve(resp);
+          resolve(resp.filter(function(d) {
+            return !!d.count;
+          }));
         }
       });
     });
@@ -356,7 +360,11 @@ function handleAggregation(params, done) {
       count: count,
       exchange: { currency:'XRP' },
       exchangeRate: 1,
-      components: data
+      components: data.filter(function(d) {
+        return !!d.converted_amount;
+      }).sort(function(a,b) {
+        return b.converted_amount - a.converted_amount;
+      })
     }
   }
 
