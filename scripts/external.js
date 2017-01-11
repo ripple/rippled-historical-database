501431f67f752b124e659311320377ab842c8dc9
Matthew Fettig <matthew@ripple.com>
2017-01-11 11:11:16 -0800

    add bitstamp to external market ETL script

--- scripts/external.js
+++ scripts/external.js
@@ -20,6 +20,90 @@ function round(n, sig) {
 }
 
 /**
+ * getBitstamp
+ */
+
+function getBitstamp(currency) {
+
+  var pair = ('xrp' + currency).toLowerCase()
+  var url = 'https://www.bitstamp.net/api/v2/transactions/' + pair
+
+
+  return request({
+    url: url,
+    json: true,
+    qs: {
+      time: 'hour'
+    }
+  }).then(function(resp) {
+    var buckets = {}
+
+    resp.forEach(function(d) {
+      var bucket = moment.unix(d.date).utc()
+      var price = Number(d.price)
+      var amount = Number(d.amount)
+
+      bucket = bucket.startOf('minute')
+      .subtract(bucket.minutes() % 5, 'minute')
+      .format('YYYY-MM-DDTHH:mm:ss[Z]')
+
+      if (!buckets[bucket]) {
+        buckets[bucket] = {
+          base_volume: 0,
+          counter_volume: 0,
+          count: 0,
+          buy_volume: 0,
+          sell_volume: 0,
+          buy_count: 0,
+          sell_count: 0,
+          open: price,
+          high: price,
+          low: price,
+          close: price
+        }
+      }
+
+      if (price > buckets[bucket].high) {
+        buckets[bucket].high = price
+      }
+
+      if (price < buckets[bucket].low) {
+        buckets[bucket].low = price
+      }
+
+
+      buckets[bucket].close = price
+      buckets[bucket].base_volume += amount
+      buckets[bucket].counter_volume += amount * price
+      buckets[bucket].count++
+
+      if (d.type === '1') {
+        buckets[bucket].sell_volume += amount
+        buckets[bucket].sell_count++
+      } else {
+        buckets[bucket].buy_volume += amount
+        buckets[bucket].buy_count++
+      }
+    })
+
+    var results = Object.keys(buckets).map(function(key) {
+      var row = buckets[key]
+      row.source = 'bitstamp.net'
+      row.interval = '5minute'
+      row.base_currency = 'XRP'
+      row.counter_currency = currency
+      row.date = key
+      row.vwap = row.counter_volume / row.base_volume
+      row.vwap = round(row.vwap, 6)
+      return row
+    })
+
+    console.log('bitstamp.net', results.length)
+    return results
+  })
+}
+
+/**
  * getBTC38
  */
 
@@ -342,7 +426,11 @@ function save(data) {
         low: r.low,
         close: r.close,
         vwap: r.vwap,
-        count: r.count
+        count: r.count,
+        buy_count: r.buy_count,
+        sell_count: r.sell_count,
+        buy_volume: r.buy_volume,
+        sell_volume: r.sell_volume
       }
     })
   })
@@ -360,6 +448,8 @@ function save(data) {
 
 function savePeriod(period, increment) {
   var markets = [
+    'bitstamp.net|XRP|USD',
+    'bitstamp.net|XRP|EUR',
     'bittrex.com|XRP|BTC',
     'poloniex.com|XRP|BTC',
     'poloniex.com|XRP|USD',
@@ -430,6 +520,8 @@ function savePeriod(period, increment) {
 }
 
 Promise.all([
+  getBitstamp('USD'),
+  getBitstamp('EUR'),
   getBTC38('CNY'),
   getBTC38('BTC'),
   getPoloniex('BTC'),
