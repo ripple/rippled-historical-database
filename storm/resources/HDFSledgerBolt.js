48ab0b2427f7eb0e2acf6190c1d536498dab30a2
Matthew Fettig <matthew@ripple.com>
2017-08-21 13:29:54 -0700

    cleanup and log scan ID

--- storm/resources/HDFSledgerBolt.js
+++ storm/resources/HDFSledgerBolt.js
@@ -1,43 +1,48 @@
+'use strict'
+
 var http = require('http')
 var config = require('./config')
-var Storm = require('./storm');
+var Storm = require('./storm')
 var hdfs = require('./lib/hdfs')
 var BasicBolt = Storm.BasicBolt
-var bolt;
+var bolt
 
-http.globalAgent.maxSockets = 2000;
+http.globalAgent.maxSockets = 2000
 
-var Logger = require('./lib/logger');
+var Logger = require('./lib/logger')
 var log = new Logger({
   scope: 'hdfs-transaction-bolt',
   file: config.get('logFile'),
   level: config.get('logLevel')
-});
+})
 
 // handle uncaught exceptions
-require('./exception')(log);
+require('./exception')(log)
 
 
 function LedgerBolt() {
-  BasicBolt.call(this);
+  BasicBolt.call(this)
 }
 
-LedgerBolt.prototype = Object.create(BasicBolt.prototype);
-LedgerBolt.prototype.constructor = LedgerBolt;
+LedgerBolt.prototype = Object.create(BasicBolt.prototype)
+LedgerBolt.prototype.constructor = LedgerBolt
 
 LedgerBolt.prototype.process = function(tup, done) {
-  var self = this;
-  var ledger = tup.values[0];
-  var d = Date.now();
+  var self = this
+  var ledger = tup.values[0]
+  var d = Date.now()
 
   hdfs.ingestLedgerHeader(ledger)
   .then(function() {
     d = (Date.now() - d) / 1000
-    self.log('HDFS ledger saved: ' + ledger.ledger_index + ' in ' + d + 's');
+    self.log('HDFS ledger saved: ' + ledger.ledger_index + ' in ' + d + 's')
     done()
   })
-  .catch(done)
-};
+  .catch(function(e) {
+    self.log(e)
+    done(e)
+  })
+}
 
-bolt = new LedgerBolt();
-bolt.run();
+bolt = new LedgerBolt()
+bolt.run()
--- storm/resources/HDFStransactionBolt.js
+++ storm/resources/HDFStransactionBolt.js
@@ -1,43 +1,49 @@
+'use strict'
+
 var http = require('http')
 var config = require('./config')
-var Storm = require('./storm');
+var Storm = require('./storm')
 var hdfs = require('./lib/hdfs')
 var BasicBolt = Storm.BasicBolt
-var bolt;
+var bolt
 
-http.globalAgent.maxSockets = 2000;
+http.globalAgent.maxSockets = 2000
 
-var Logger = require('./lib/logger');
+var Logger = require('./lib/logger')
 var log = new Logger({
   scope: 'hdfs-transaction-bolt',
   file: config.get('logFile'),
   level: config.get('logLevel')
-});
+})
 
 // handle uncaught exceptions
-require('./exception')(log);
+require('./exception')(log)
 
 
 function TransactionBolt() {
-  BasicBolt.call(this);
+  BasicBolt.call(this)
 }
 
-TransactionBolt.prototype = Object.create(BasicBolt.prototype);
-TransactionBolt.prototype.constructor = TransactionBolt;
+TransactionBolt.prototype = Object.create(BasicBolt.prototype)
+TransactionBolt.prototype.constructor = TransactionBolt
 
 TransactionBolt.prototype.process = function(tup, done) {
-  var self = this;
-  var tx = tup.values[0];
-  var d = Date.now();
+  var self = this
+  var tx = tup.values[0]
+  var d = Date.now()
 
   hdfs.ingestTransaction(tx)
   .then(function() {
     d = (Date.now() - d) / 1000
-    self.log('tx HDFS: ' + tx.ledger_index + '.' + tx.tx_index + ' saved in ' + d + 's')
+    self.log('tx HDFS: ' + tx.ledger_index + '.' +
+             tx.tx_index + ' saved in ' + d + 's')
     done()
   })
-  .catch(done)
-};
+  .catch(function(e) {
+    self.log(e)
+    done(e)
+  })
+}
 
-bolt = new TransactionBolt();
-bolt.run();
+bolt = new TransactionBolt()
+bolt.run()
